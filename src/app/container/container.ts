996b5d8c3ea395807b93026b73838b1894a3207a
Bhavika <bhavikakriplani2002@gmail.com>
2025-10-09 14:11:48 +0530

    Push to git

---
+++ src/app/container/container.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Employee } from '../employee/employee';
+import { Room } from '../rooms/roomsInterface';
+
+@Component({
+  selector: 'app-container',
+  imports: [],
+  templateUrl: './container.html',
+  styleUrl: './container.scss'
+})
+export class Container {
+
+}
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header {
+export class Header implements OnInit {
 
   constructor() {}
+  ngOnInit() : void{
+
+  }
 }
